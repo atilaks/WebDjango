950215253bc85253b8d78a20d187bc5a16b238f5
jaime <jaimefrancotorrecillas@gmail.com>
2022-09-11 08:56:50 +0200

    act

--- WebDjango/gestionPedidos/admin.py
+++ WebDjango/gestionPedidos/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from gestionPedidos.models import Clientes
+from gestionPedidos.models import Clientes, Articulos, Pedidos
 # Register your models here.
 
-admin.site.register(Clientes)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'telefono')
+    search_fields = ('nombre', 'telefono')
+
+class ArticulosAdmin(admin.ModelAdmin):
+    list_filter = ('seccion',)
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'fecha')
+    list_filter = ('fecha',)
+    date_hierarchy = 'fecha'
+
+admin.site.register(Clientes, ClienteAdmin)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos, PedidosAdmin)
